8bbe895a4da80cc4cfd55a0745f2ae053115066a
Aureliusf <44497836+Aureliusf@users.noreply.github.com>
2020-02-03 07:51:16 +0100

    Testing song features

--- album.py
+++ album.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib as plt
 import requests
 
 id = '3yj1EziuVuch1OayyM95az'
@@ -6,52 +7,35 @@ url = 'https://api.spotify.com/v1/albums/{}/tracks'
 headers = {'Authorization':'Bearer {}'}
 
 abulm = requests.get('https://api.spotify.com/v1/albums/{}/tracks'.format(id),headers=headers)
+song = requests.get('https://api.spotify.com/v1/audio-features/{}',headers=headers)
 data = json.loads(abulm.text)
 uris = []
 song_titles = []
 
-
-danceability = []
-energy = []
-key = []
-loudness = []
-mode = []
-speechiness = []
-acousticness = []
-instrumentalness = []
-liveness = []
-valence = []
-tempo = []
-duration_ms = []
-
-
-
 # Get songs from album
 
-for n in range(1,len(data['items'])):
+for n in range(1,len(data)):
     uri = data['items'][n]['uri']
     uri = uri[14:len(uri)]
     uris.append(uri)
     song_titles.append(data['items'][n]['name'])
 print(uris)
 
-# Get songs stats
-for n in range(1,len(uris)):
-    id = uris[n]
-    #print(uris[n])
-    analisis = json.loads(requests.get('https://api.spotify.com/v1/audio-features/{}'.format(id),headers=headers).text)
-    print(analisis["danceability"])
-	danceability.append(analisis["danceability"])
-	energy.append(analisis['energy'])
-	key.append(analisis['key'])
-	loudness.append(analisis['loudness'])
-	mode.append(analisis['mode'])
-	speechiness.append(analisis['speechiness'])
-	acousticness.append(analisis['acousticness'])
-	instrumentalness.append(analisis['instrumentalness'])
-	liveness.append(analisis['liveness'])
-	valence.append(analisis['valence'])
-	tempo.append(analisis['tempo'])
-	duration_ms.append(analisis['duration_ms'])
+# Get song features
+for n in range (0,len(uris)):
+    song  = requests.get('https://api.spotify.com/v1/audio-features/{}'.format(uris[n]),headers=headers)
+    #print(song.text)
+    # Loads data into the fixed structure below for every song
+    features = json.loads(song.text)
+    features_clean = [{'name':'danceability','value':0},{'name':'energy','value':0},{'name':'speechiness','value':0},{'name':'acousticness','value':0},{'name':'instrumentalness','value':0},{'name':'liveness','value':0},{'name':'valence','value':0}]
+ 
+    for n in range(0,len(features_clean)):
+        print(features[features_clean[n]['name']])
+        features_clean[n]['value'] = features[features_clean[n]['name']]
+    album[n]['features'] = features_clean
+ 
+    print(features_clean)
 
-print(song_titles[1],danceability[1])
+
+
+
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import matplotlib.pyplot as plt
+
 id = []
 id.append('4V3PsyJJOVXw1SNm7pJh6e')
 id.append('3yj1EziuVuch1OayyM95az')
@@ -8,6 +10,38 @@ headers = {'Authorization':'Bearer {}'}
 
 song = requests.get('https://api.spotify.com/v1/audio-features/{}'.format(id[0]),headers=headers)
 
-print(song.text)
+#print(song.text)
+
+# Loads data into the fixed structure below
+features = json.loads(song.text)
+features_clean = [{'name':'danceability','value':0},{'name':'energy','value':0},{'name':'speechiness','value':0},{'name':'acousticness','value':0},{'name':'instrumentalness','value':0},{'name':'liveness','value':0},{'name':'valence','value':0}]
+
+for n in range(0,len(features_clean)):
+    print(features[features_clean[n]['name']])
+    features_clean[n]['value'] = features[features_clean[n]['name']]
+
+#print(features_clean)
+
+
+# Title compose
+values = []
+for n in range(0,len(features_clean)):
+    values.append(features_clean[n]['value'])
+max_index = values.index(max(values))
+title = 'This song is big on {}'
+
+# Plotting
+x = [1,2,3,4,5,6,7]
+y = [features_clean[0]['value'],features_clean[1]['value'],features_clean[2]['value'],features_clean[3]['value'],features_clean[4]['value'],features_clean[5]['value'],features_clean[6]['value']]
+
+plt.scatter(x,y,s=30)
+plt.title(title.format(features_clean[max_index]['name']))
+plt.ylim(top=1)
+
+    # Annotation system, i know its a bad
+for n in range(0,len(features_clean)):
+    plt.annotate(features_clean[n]['name'],((n+1),features_clean[n]['value']))
+
+plt.show()
 
 
